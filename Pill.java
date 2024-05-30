b98e54b45392f019bd512e018c5f5df7a81a9681
GTRebeta <rebetagt@appstate.edu>
2024-05-30 07:34:08 -0400

    Fleshed out the abstract Pill superclass.

--- Pill.java
+++ Pill.java
@@ -1,8 +1,27 @@
 ////////////////////////////////////////////////////////////////
+package gitPractice;
 /**
  * The Pill abstract superclass.
  * 
  * @author Gannon Rebeta
- * @version 5/29/2024
+ * @version 5/30/2024
  */
+public abstract class Pill
+{
+    protected String color;
+    /**
+     * 1-arg constructor.
+     * 
+     * @param color red or blue
+     */
+    public Pill(String color)
+    {
+        this.color = color;
+    }
+
+    /**
+     * Prints specialized message. Implemented by RedPill & BluePill.
+     */
+    public abstract void message();
+}
 ////////////////////////////////////////////////////////////////
