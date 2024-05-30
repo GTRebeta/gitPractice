a79bc4b5e11f55285a79f999b135920159513364
GTRebeta <rebetagt@appstate.edu>
2024-05-30 08:07:46 -0400

    Fleshed out the RedPill & BluePill subclasses.

--- BluePill.java
+++ BluePill.java
@@ -1,8 +1,28 @@
 ////////////////////////////////////////////////////////////////
+package gitPractice;
 /**
  * The BluePill subclass.
  * 
  * @author Gannon Rebeta
- * @version 5/29/2024
+ * @version 5/30/2024
  */
+public class BluePill extends Pill
+{
+    /**
+     * No-arg constructor. Color passed to Pill superclass.
+     */
+    public BluePill()
+    {
+        super("blue");
+    }
+
+    /**
+     * Prints comforting message.
+     */
+    public void message()
+    {
+        System.out.println("You awake in your bed, believing"
+                           + "whatever you want to believe.");
+    }
+}
 ////////////////////////////////////////////////////////////////
--- RedPill.java
+++ RedPill.java
@@ -1,8 +1,27 @@
 ////////////////////////////////////////////////////////////////
+package gitPractice;
 /**
  * The RedPill subclass.
  * 
  * @author Gannon Rebeta
- * @version 5/29/2024
+ * @version 5/30/2024
  */
+public class RedPill extends Pill
+{
+    /**
+     * No-arg constructor. Color passed to Pill superclass.
+     */
+    public RedPill()
+    {
+        super("red");
+    }
+
+    /**
+     * Prints liberating message.
+     */
+    public void message()
+    {
+        System.out.println("You awoke in the Matrix.");
+    }
+}
 ////////////////////////////////////////////////////////////////
